c4e929ae183c928abe5fa44f34d7720bf6dfe7de
Suraj Vishwakarma <surajv705266@gmail.com>
2026-07-14 23:48:23 +0530

    make somei

--- app.js
+++ app.js
@@ -13,19 +13,26 @@ connectDB();
 // app.use(cors());
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://expensetrackertas.netlify.app",
+  "https://expensetrackertas.netlify.app"
 ];
+
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS: " + origin));
+
+      if (!origin) {
+        return callback(null, true);
       }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
     },
-    credentials: true,
+
+    credentials: true
   })
 );
 
